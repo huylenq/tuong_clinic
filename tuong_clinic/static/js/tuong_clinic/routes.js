5237f32d5a838b1c1a067646434880e0b49b34b5
Ubuntu <ubuntu@ip-172-31-19-67.ap-southeast-1.compute.internal>
2017-07-29 11:09:14 +0000

    Add Trello link

--- tuong_clinic/static/js/tuong_clinic/routes.js
+++ tuong_clinic/static/js/tuong_clinic/routes.js
@@ -1,15 +1,11 @@
 var app = angular.module('opal');
 app.controller('WelcomeCtrl', function(){});
 
+console.log("Route setup");
+
 app.config(
     ['$routeProvider',
      function($routeProvider){
-//	     $routeProvider.when('/',  {redirectTo: '/list'})
-
-         $routeProvider.when('/',  {
-             controller: 'WelcomeCtrl',
-             templateUrl: '/templates/welcome.html'}
-                            )
-
+	 $routeProvider.when('/',  {redirectTo: '/list'})
      }]);
 
